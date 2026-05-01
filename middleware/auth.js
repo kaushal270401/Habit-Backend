eb9f718da8369df772e28ade80978de46998f8fa
kaushal <kaushal.k@idigicloudtech.com>
2026-05-02 02:13:03 +0530

    auth file chnages

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,15 @@
 import { expressjwt } from "express-jwt";
 import jwkRsa from 'jwks-rsa';
 
+const KEYCLOAK_URL = process.env.KEYCLOAK_URL || "https://habit-keycloak-1.onrender.com";
+
 export const checkJwt = expressjwt({
     secret: jwkRsa.expressJwtSecret({
         cache:true,
         rateLimit:true,
-        jwksUri:"http://localhost:8080/realms/habit-app/protocol/openid-connect/certs"
+        jwksUri:`${KEYCLOAK_URL}/realms/habit-app/protocol/openid-connect/certs`
     }),
     audience:"account",
-    issuer:"http://localhost:8080/realms/habit-app",
+    issuer:`${KEYCLOAK_URL}/realms/habit-app`,
     algorithms:["RS256"]
 })
